17da0f6dc6314e2a3cc84e6c43dfb5a51e3f4671
Ken Pham <kpham@eyeprogpo.com>
2023-02-17 22:59:41 -0500

    First commit

---
+++ src/test/java/vupham0402/spring_data_jpa/SpringDataJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package vupham0402.spring_data_jpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDataJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
